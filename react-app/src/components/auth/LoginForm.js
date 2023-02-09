a4283145b70a03aafe0ac350c9d3e5c100c9f53c
Dusty Mumphrey <dusty@clockwork.com>
2023-02-09 09:00:54 -0600

    added google sign in

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { GoogleLogin } from '@react-oauth/google';
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +34,57 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
+    <div className='login'>
+      <form className='login__form' onSubmit={onLogin}>
+        <h2 className='login__form-title'>Log In</h2>
+        <div className='login__errors'>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className='login__input-section'>
+          <label className='login__label' htmlFor='email'>Email</label>
+          <input
+            className='login__input'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className='login__input-section'>
+          <label className='login__label' htmlFor='password'>Password</label>
+          <input
+            className='login__input'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <p className='login__forgot-pass'>Forgot password?</p>
+        <button className='login__submit' type='submit'>Login</button>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
         />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+        <div className='login__signup-con'>
+            <p>No account? Sign up using:</p>
+            <div>
+              <FontAwesomeIcon icon={faCircle} />
+            </div>
+            <p>Or create an account here</p>
+
+        </div>
+
+      </form>
+    </div>
   );
 };
 
